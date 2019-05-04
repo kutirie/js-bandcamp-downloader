af9ca42e877d375917e47042fbaba8e3030acf9c
Work <user@MacBook-Air-User.local>
2019-05-05 01:18:55 +0500

    get_url

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-hello world
+function get_url(bc_link) {
+	$.get(bc_link, (data) => {
+		let pattern = /"is_downloadable":false,"file":{"mp3-128":".+"},"video_mobile_url"/ig;
+		
+		setTimeout(() => {
+			let result = data.match(pattern);
+			let url = result.toString().replace('"is_downloadable":false,"file":{"mp3-128":"', '').replace('"},"video_mobile_url"', '');
+			console.log(url);	
+		}, 3000)
+	});
+}
