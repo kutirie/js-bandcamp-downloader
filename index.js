d9848654b6d7b29d25acef42e1527d55c56560fa
Work <user@MacBook-Air-User.local>
2019-05-05 19:03:38 +0500

    work script

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-function get_url(bc_link) {
+get_url = (bc_link) => {
 	$.get(bc_link, (data) => {
 		let pattern = /"is_downloadable":false,"file":{"mp3-128":".+"},"video_mobile_url"/ig;
 		
-		setTimeout(() => {
-			let result = data.match(pattern);
-			let url = result.toString().replace('"is_downloadable":false,"file":{"mp3-128":"', '').replace('"},"video_mobile_url"', '');
-			console.log(url);	
-		}, 3000)
+		let result = data.match(pattern);
+		let url = result.toString().replace('"is_downloadable":false,"file":{"mp3-128":"', '').replace('"},"video_mobile_url"', '');
+		console.log(url);
+		$('#download_container').append('<a href="' + url + '" target="_blank">Download</a>');	
 	});
 }
+
+$('#bc_submit').click(() => {
+	console.log('start')
+	get_url($('#bc_url').val())
+})
